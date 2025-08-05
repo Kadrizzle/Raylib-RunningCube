ad424581a6cc60b0476a47ecccb794f585d7ea03
Kadrizzle <lanekade8@gmail.com>
2025-08-05 13:47:20 -0500

    Main menu and level function

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,68 @@
 #include <raylib.h>
-#include "ball.h"
 
-int main() 
+int main()
 {
-    const Color darkGreen = {20, 160, 133, 255};
-    
+    const Color navyBlue = {0, 0, 128, 255};
+
     constexpr int screenWidth = 800;
     constexpr int screenHeight = 600;
-    
-    Ball ball;
-    
-    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
+
+    int currentLevel = 0;
+
+    InitWindow(screenWidth, screenHeight, "Square Runner");
     SetTargetFPS(60);
-    
+
     while (!WindowShouldClose())
     {
-        ball.Update();
-        
+        switch (currentLevel)
+        {
+        case 0:
+
+            break;
+        case 1:
+
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+        case 4:
+
+            break;
+        }
+
         BeginDrawing();
-            ClearBackground(darkGreen);
-            ball.Draw();
+
+        switch (currentLevel)
+        {
+        case 0:
+            DrawText("Square Runner", (screenWidth / 2) - 150, screenHeight - (screenHeight - 20), 45, navyBlue);
+            DrawText("Press the enter key to start the game", (screenWidth / 2) - 300, screenHeight - (screenHeight - 80), 30, navyBlue);
+
+            if (IsKeyPressed(KEY_ENTER))
+            {
+                currentLevel = 1;
+            }
+
+            break;
+        case 1:
+            DrawText("Hello, you're on level 1", 50, 50, 50, navyBlue);
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+        case 4:
+
+            break;
+        }
+        ClearBackground(BEIGE);
         EndDrawing();
     }
-    
+
     CloseWindow();
 }
